f6fe9c7f4e9e6577a07f6c94a6f2265bcff09de2
agbautista <archiebautista@live.com>
2014-08-01 11:49:43 +0800

    Day 1,
    Working on centering the frame.

--- javascript/script.js
+++ javascript/script.js
@@ -1,6 +1,47 @@
 function getScreenSize(){
-    var winHt, winWt;
+    var winHt, winWt,posX, posY;
     winHt = parseInt($(window).innerHeight());
     winWt = parseInt($(window).innerWidth());
-    alert(winHt + " and " + winWt);
+
+    if (winWt > 1280){
+        if (winHt > 800){
+            //Therefore Kasya yung w and h ng go astral. Igigitna na lng.
+            posX = (winWt * .5) - (640);
+            posY = (winHt * .5) - 400;
+            $("#mainFrame").css({
+                "position" : "absolute",
+                "left" : posX + "px",
+                "top" : posY + "px",
+                "width" : "1280px",
+                "height" : "800px"
+            });
+        }
+        else{
+            //therefore kasya yung haba pero hinde yung tnagkad. Shrink this shit.
+            posX = (winWt * .5) - (parseInt(shrinkWidth(winHt)) * .5);
+            alert(posX);
+            $("#mainFrame").css({
+                "position": "absolute",
+                "left": posX + "px",
+                "top": 0,
+                "width": shrinkWidth(winHt) + "px",
+                "height": winHt + "px"
+            });
+            alert($("#mainFrame").css("left"));
+        }
+    }
+    else{
+        if (winHt > 800) {
+            //Possible ba na magkasya yung tangkad pero hinde yung haba? anyways shrink this shit.
+        }
+        else {
+            //therefore maliit yung monitor. Shrink this shit. adopt the height of the monitor. Adjust the width of frame.
+        }
+    }
+}
+function shrinkWidth(ht){
+    return ((parseInt(ht) * 1280) / 800);
+}
+function shrinkHeight(wt){
+    return ((parseInt(wt) * 800) / 1280);
 }
