d1b34f128ab71ba5a9666fff42f9635edf018fb8
agbautista <archiebautista@live.com>
2014-08-01 10:55:45 +0800

    Day 1 10:55 am

---
+++ javascript/script.js
@@ -0,0 +1,6 @@
+function getScreenSize(){
+    var winHt, winWt;
+    winHt = parseInt($(window).innerHeight());
+    winWt = parseInt($(window).innerWidth());
+    alert(winHt + " and " + winWt);
+}
